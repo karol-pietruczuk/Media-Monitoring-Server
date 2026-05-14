58eddef7491ff5796794a39d2ae132f20b2cd77c
Karol Pietruczuk <karol.pietruczuk@gmail.com>
2026-05-14 08:47:30 +0200

    Added tota-data-channel-history.entity.ts without relations

---
+++ src/core/type/total-data-channel-change.ts
@@ -0,0 +1,7 @@
+export type TotalDataChannelChangeType =
+  | 'Created totalDataChannel'
+  | 'Changed dataMappingInfo'
+  | 'Changed meter'
+  | 'Changed dataSource'
+  | 'Changed createdAt'
+  | 'Changed several Values';
