8e359a07731b613a28daea432856e92bbfdb3709
Karol Pietruczuk <KP7121@wipasz.internal>
2026-05-14 09:48:15 +0200

    Added location-history.entity.ts

---
+++ src/domain/location/entities/location-history.entity.ts
@@ -0,0 +1,41 @@
+import { Column, Entity, Index, PrimaryGeneratedColumn } from 'typeorm';
+import type { LocationChangetype } from '../../../core/type/location-change-type';
+
+@Index('PK_LOCATION_HISTORY', ['id'], { unique: true })
+@Entity('locationHistory', { schema: 'dbo' })
+export class LocationHistory {
+  @PrimaryGeneratedColumn({ type: 'int', name: 'id' })
+  id!: number;
+
+  @Column({
+    type: 'nvarchar',
+    name: 'locationChangetype',
+    length: 30,
+  })
+  locationChangetype!: LocationChangetype;
+
+  @Column({ type: 'int', name: 'locationId' })
+  locationId!: number;
+
+  @Column('nvarchar', { name: 'locationMainLocation', length: 50 })
+  locationMainLocation!: string;
+
+  @Column('nvarchar', {
+    name: 'locationSubLocation',
+    nullable: true,
+    length: 50,
+  })
+  locationSubLocation!: string | null;
+
+  @Column('datetime', {
+    name: 'locationCreatedAt',
+    default: () => 'getdate()',
+  })
+  locationCreatedAt!: Date;
+
+  @Column('datetime', {
+    name: 'createdAt',
+    default: () => 'getdate()',
+  })
+  createdAt!: Date;
+}
