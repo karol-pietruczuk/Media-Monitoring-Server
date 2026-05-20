6a98190c95e39aee68048c1e81301b61dbfd6181
Karol Pietruczuk <karol.pietruczuk@gmail.com>
2026-05-20 12:29:52 +0200

    location problems

--- src/domain/location/dto/update-location.dto.ts
+++ src/domain/location/dto/update-location.dto.ts
@@ -1,9 +1,4 @@
 import { PartialType } from '@nestjs/mapped-types';
 import { CreateLocationDto } from './create-location.dto';
 
-// Rzutujemy funkcję PartialType przez as unknown, aby uciszyć regułę lintera no-unsafe-call
-const SafePartialType = PartialType as unknown as (
-  classRef: new () => CreateLocationDto,
-) => new () => Partial<CreateLocationDto>;
-
-export class UpdateLocationDto extends SafePartialType(CreateLocationDto) {}
+export class UpdateLocationDto extends PartialType(CreateLocationDto) {}
