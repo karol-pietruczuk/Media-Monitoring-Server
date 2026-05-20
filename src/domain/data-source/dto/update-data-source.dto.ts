e7292718a956495a647f90c908b5eba9ddec268e
Karol Pietruczuk <karol.pietruczuk@gmail.com>
2026-05-20 12:20:49 +0200

    Data-source problems

--- src/domain/data-source/dto/update-data-source.dto.ts
+++ src/domain/data-source/dto/update-data-source.dto.ts
@@ -1,8 +1,6 @@
 import { PartialType } from '@nestjs/mapped-types';
 import { CreateDataSourceDto } from './create-data-source.dto';
 
-const SafePartialType = PartialType as unknown as (
-  classRef: new () => CreateDataSourceDto,
-) => new () => Partial<CreateDataSourceDto>;
-
-export class UpdateDataSourceDto extends SafePartialType(CreateDataSourceDto) {}
+// Oczyszczono z obejścia (hacku) "SafePartialType". Standardowe użycie PartialType z NestJS
+// w pełni poprawnie mapuje opcjonalne pola i jest bezpieczne typologicznie.
+export class UpdateDataSourceDto extends PartialType(CreateDataSourceDto) {}
